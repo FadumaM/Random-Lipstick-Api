889bf4934e10b2b90a693ec3f6d6d1055b0f887c
FadumaM <faduma.mohammed@gmail.com>
2016-07-17 20:00:18 +0100

    Treying to fix the regex for the lipstick name

--- index.js
+++ index.js
@@ -1,49 +1,48 @@
 var rp = require("request-promise");
-var cheerio  = require("cheerio");
+var cheerio = require("cheerio");
 var mongoose = require("mongoose");
-var config   = require("./config/config");
+var config = require("./config/config");
 
 
-mongoose.connect(config.database, function(){
-  console.log("Connected with database");
+mongoose.connect(config.database, function() {
+    console.log("Connected with database");
 });
 
 
-var Colorurl   = "http://www.maccosmetics.co.uk/product/13854/310/Products/Makeup/Lips/Lipstick/Lipstick";
+var Colorurl = "http://www.maccosmetics.co.uk/product/13854/310/Products/Makeup/Lips/Lipstick/Lipstick";
 
 var Nameurl = "http://www.maccosmetics.co.uk/product/13854/310/Products/Makeup/Lips/Lipstick/Lipstick#/shade/Finally_Free_%28ONLINE_EXCLUSIVE%29";
 
 getColor(Colorurl);
-getName(Nameurl);
+// getName(Nameurl);
 
 function getColor(Colorurl) {
-  return rp(Colorurl)
+    return rp(Colorurl)
     .then(function(body) {
-      var $      = cheerio.load(body);
-      var lipsticks;
-      $("select").each(function() {
-        if ($(this).toString().indexOf("data-product") > -1) lipsticks = $(this).text(); {
-          var lipsticksColor = lipsticks.match(/(?:#|0x)?(?:[0-9a-f]{2}){3,4}/ig);
-          console.log(lipsticksColor);
-        }
-      });
-    })
-    .catch(function(err) {
-      console.log("something went wrong...", err);
-    });
-}
+            var $ = cheerio.load(body);
+            var lipsticks = [];
+            $("select").each(function() {
+                lipsticks = $(this).text();
+                var lipsticksColor = lipsticks.match(/(?:#|0x)?(?:[0-9a-f]{2}){3,4}/ig);
+                // console.log(lipsticksColor);
+                return rp(Nameurl)
+                .then(function(body) {
+                    var $ = cheerio.load(body);
+                    var lipsticks = [];
+                    var lipsticksName = [];
+                    $("div.shade-picker-float__colors").each(function() {
+                        lipsticks = $(this).toString();
+                        var name  = lipsticks.match(/(>([^>]+)<)/ig);
+                        console.log(name);
 
-function getName(Nameurl) {
-  return rp(Nameurl)
-    .then(function(body) {
-      var $      = cheerio.load(body);
-      var lipsticksName;
-      $("div.shade-picker-float__colors").each(function() {
-        lipsticksName = $(this).text();
-        console.log(lipsticksName);
-      });
-    })
-    .catch(function(err) {
-      console.log("something went wrong...", err);
-    });
+                    });
+                });
+            });
+        })
+        .catch(function(err) {
+                console.log("something went wrong...", err);  });
 }
+// (&nbsp;|>([^>]+)>)
+// (>([^>]+)<)
+//
+//
